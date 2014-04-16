47f84b62fa0dd1b55172d7e6aeaca9fdd180c22a
mark-otaris <mark.otaris@gmail.com>
2014-04-16 22:25:50 +0000

    Update all the library scripts to their latest versions.
    The library scripts were outdated and needed to be updated to their latest versions, here fetched from the website.

--- library-scripts/RbxGear-45374389.lua
+++ library-scripts/RbxGear-45374389.lua
@@ -1,25 +1,10 @@
---rbxsig%pR1KL5JTxcMTnwr9RQZkuLQk8eyx+s2DEW3/pEegIuri0K5LTsHfwBpfq11iqbQ0Pu56t192b56L7hUD4zc6O4Glu9V6qGKn6m+1RJgbzRZAj1GZrjwDmQTBt1U2NmTN4S8miYAlC6jAQLXJRHrKpwTVNK0nxbKGxzQ97BdtpZQ=%
---rbxassetid%45374389%
-local t = {}
-
-t.Foo = 
-	function() 
-		print("foo") 
-	end
-
-t.Bar = 
-	function() 
-		print("bar") 
-	end
-
-t.Help = 
-	function(funcNameOrFunc) 
-		--input argument can be a string or a function.  Should return a description (of arguments and expected side effects)
-		if funcNameOrFunc == "Foo" or funcNameOrFunc == t.Foo then 
-			return "Function Foo.  Arguments: None.  Side effect: prints foo" 
-		elseif funcNameOrFunc == "Bar" or funcNameOrFunc == t.Bar then 
-			return "Function Bar.  Arguments: None.  Side effect: prints bar" 
-		end 
-	end
-
-return t
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>403 Forbidden</title>
+</head><body>
+<h1>Forbidden</h1>
+<p>You don't have permission to access /get.php
+on this server.</p>
+<p>Additionally, a 403 Forbidden
+error was encountered while trying to use an ErrorDocument to handle the request.</p>
+</body></html>
